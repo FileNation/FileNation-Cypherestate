3c43049792c6e60a9c092db625413b6b808e6fd8
PolBaladas <polbaladasluna@gmail.com>
2017-10-15 03:22:48 +0200

    Direct to submission after blog post

--- main.py
+++ main.py
@@ -15,67 +15,82 @@ migrate = Migrate(app, db)
 from utils import models, db_handler
 db.create_all()
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about/')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/donate/')
 def donate():
-	return render_template('donate.html')
+    return render_template('donate.html')
+
 
 @app.route('/submission/')
 def submission():
-	return render_template('submission.html')
+    return render_template('submission.html')
 
-@app.route('/author/<author>')
-def author(author):
-	pass
+
+@app.route('/blog/<blog_name>/')
+def blog(blog_name):
+    blog = db_handler.getBlogByName(blog_name)
+    if blog:
+        return redirect(gateway + blog.hash + '/')
+    else:
+        return render_template('error.html', error='Bad Name')
+
 
 @app.route('/newpost/', methods=['POST'])
 def newpost():
-	form = request.form
-	text = form['text']
-	title = form['title']
-	key = form['key']
-	blog_id = db_handler.getBlogByKey(key)
-	if blog_id:
-		post_hash = db_handler.newPost(title, text, blog_id)
-		return redirect('/post/'+post_hash)
-	else:
-		return 'Sorry! Bad key.'
-		#return render_template('index.html', text=text, title=title, bad_key=True)
+    form = request.form
+    text = form['text']
+    title = form['title']
+    key = form['key']
+    blog_id = db_handler.getBlogByKey(key)
+    if blog_id:
+        post_hash = db_handler.newPost(title, text, blog_id)
+        return render_template(
+            'submission.html',
+            post_hash=post_hash,
+            blog_hash=blog.hash
+        )
+    else:
+        return render_template('error.html', error='Sorry! Bad key.')
+
 
 @app.route('/newblog/', methods=['GET', 'POST'])
 def newblog():
-	if request.method=='POST':
-		form = request.form
-		blog_name = form['blog_name']
-		author = form['author']
+    if request.method == 'POST':
+        form = request.form
+        blog_name = form['blog_name']
+        author = form['author']
 
-		error = db_handler.validateSubmission(blog_name, author)
-		if error:
-			return render_template('new_blog.html', error=error)
+        error = db_handler.validateSubmission(blog_name, author)
+        if error:
+            return render_template('error.html', error=error)
 
-		key, ipns = db_handler.newBlog(author, blog_name)
-		return render_template('new_blog.html', key=key, adress=ipns)
-	
-	else:
-		return render_template('new_blog.html')
+        key, ipns = db_handler.newBlog(author, blog_name)
+        return render_template('new_blog.html', key=key, adress=ipns)
+
+    else:
+        return render_template('new_blog.html')
 
 
 @app.route('/post/<post_hash>/')
 def post(post_hash):
-	return redirect('https://gateway.ipfs.io/ipfs/'+post_hash+'/', code=302)
+    return redirect('https://ipfs.io/ipfs/' + post_hash + '/', code=302)
+
 
 @app.route('/checkBlogName/<blog_name>/')
 def checkBlogName(blog_name):
-	return str(db_handler.blogExists(blog_name))
+    return str(db_handler.blogExists(blog_name))
 
 
 @app.route('/checkAuthor/<author>/')
 def checkAuthor(author):
-	return str(db_handler.authorExists(author))
+    return str(db_handler.authorExists(author))
--- utils/db_handler.py
+++ utils/db_handler.py
@@ -1,5 +1,5 @@
 from sqlalchemy import or_
-from main import app,db
+from main import app, db
 from .models import *
 from .values import *
 import hashlib
@@ -12,148 +12,158 @@ api = ipfsapi.connect('127.0.0.1', 5001)
 
 
 def getBlogByKey(key):
-	key = getSHA(key.encode('utf-8'))
-	blog = Blog.query.filter_by(key=key).first()
-	return blog.id if blog else False
+    key = getSHA(key.encode('utf-8'))
+    blog = Blog.query.filter_by(key=key).first()
+    return blog.id if blog else False
+
+
+def getBlogByName(name):
+    blog = Blog.query.filter_by(name=name)
+    return blog if blog else False
 
 
 def newPost(title, text, blog_id):
-	template = fillPostTemplate(title, text)
-	post_file = createPostFile(title, template)
-	post_hash = uploadPost(post_file)
-	post = addPostToDB(post_hash, blog_id, title)
-	blog = Blog.query.get(blog_id)
-	addPostToBlog(post, blog)
-	return post_hash
-	
-	
+    template = fillPostTemplate(title, text)
+    post_file = createPostFile(title, template)
+    post_hash = uploadPost(post_file)
+    post = addPostToDB(post_hash, blog_id, title)
+    blog = Blog.query.get(blog_id)
+    addPostToBlog(post, blog)
+    return post_hash
+
+
 def fillPostTemplate(title, text):
-	template = open(post_template).read()
-	filled = render_template_string(template, title=title, text=text)
-	return filled
+    template = open(post_template).read()
+    filled = render_template_string(template, title=title, text=text)
+    return filled
 
 
 def createPostFile(title, template):
-	temp_name = title.replace(' ', '-')
-	outfile = open(temp+temp_name+'.html', 'w+')
-	outfile.write(template)
-	outfile.close()
-	return temp_name
+    temp_name = title.replace(' ', '-')
+    outfile = open(temp + temp_name + '.html', 'w+')
+    outfile.write(template)
+    outfile.close()
+    return temp_name
 
 
 def uploadPost(post_file):
-	res = api.add(temp+post_file+'.html')
-	post_hash = res['Hash']
-	return post_hash
+    res = api.add(temp + post_file + '.html')
+    post_hash = res['Hash']
+    return post_hash
 
 
 def addPostToDB(post_hash, blog_id, title):
-	post = Post(post_hash, blog_id, title)
-	db.session.add(post)
-	db.session.commit()
-	return post
+    post = Post(post_hash, blog_id, title)
+    db.session.add(post)
+    db.session.commit()
+    return post
 
 
 def newBlog(author, name):
-	template = fillBlogTemplate(author, name)
-	blog_file = createBlogFile(name, template)
-	blog_hash = uploadBlog(blog_file)
-	key = generateBlogKey() 
-	hashed_key = getSHA(key)
-	addBlogToDB(blog_hash, hashed_key, name, author)
-	return key
+    template = fillBlogTemplate(author, name)
+    blog_file = createBlogFile(name, template)
+    blog_hash = uploadBlog(blog_file)
+    key = generateBlogKey()
+    hashed_key = getSHA(key)
+    addBlogToDB(blog_hash, hashed_key, name, author)
+    return key
+
 
 def addPostToBlog(post, blog):
-	template = fillBlogPosts(blog)
-	file = createBlogFile(blog.name, template)
-	blog_hash = uploadBlog(file)
-	blog.hash = blog_hash
-	db.session.commit()
+    template = fillBlogPosts(blog)
+    file = createBlogFile(blog.name, template)
+    blog_hash = uploadBlog(file)
+    blog.hash = blog_hash
+    db.session.commit()
+
 
 def fillBlogPosts(blog):
-	posts = Post.query.filter_by(blog_id=blog.id)
-	template = open(blog_template).read()
-	filled = render_template_string(
-		 template,
-		 blog = blog,
-		 posts = posts
-	)
-	return filled
+    posts = Post.query.filter_by(blog_id=blog.id)
+    template = open(blog_template).read()
+    filled = render_template_string(
+        template,
+        blog=blog,
+        posts=posts
+    )
+    return filled
+
 
 def fillBlogTemplate(author, name):
-	template = open(blog_template).read()
-	filled = render_template_string(
-		template,
-	 	title = title, 
-	 	author = author
-	 )
-	return filled
+    template = open(blog_template).read()
+    filled = render_template_string(
+        template,
+        title=title,
+        author=author
+    )
+    return filled
 
 
 def createBlogFile(name, template):
-	temp_name = name.replace(' ','-')
-	outfile = open(temp_blogs+temp_name+'.html', 'w+')
-	outfile.write(template)
-	outfile.close()
-	return temp_name
+    temp_name = name.replace(' ', '-')
+    outfile = open(temp_blogs + temp_name + '.html', 'w+')
+    outfile.write(template)
+    outfile.close()
+    return temp_name
+
 
 def uploadBlog(blog_file):
-	res = api.add(temp_blogs+blog_file+'.html')
-	#ipfs_path = '/ipfs/'+res['Hash']
-	#published_data = api.name_publish(ipfs_path, resolve=True, lifetime='175200h')
-	return res['Hash']
+    res = api.add(temp_blogs + blog_file + '.html')
+    #ipfs_path = '/ipfs/'+res['Hash']
+    #published_data = api.name_publish(ipfs_path, resolve=True, lifetime='175200h')
+    return res['Hash']
 
 
 def generateBlogKey():
-	key = generateKey()
-	if keyExists(key):
-		generateBlogKey()
-	return key
+    key = generateKey()
+    if keyExists(key):
+        generateBlogKey()
+    return key
 
 
 def addBlogToDB(blog_hash, key, name, author):
-	blog = Blog(blog_hash, key, name, author)
-	db.session.add(blog)
-	db.session.commit()
+    blog = Blog(blog_hash, key, name, author)
+    db.session.add(blog)
+    db.session.commit()
 
 
 def generateKey():
-	rand = os.urandom(24)
-	return getSHA(rand)
+    rand = os.urandom(24)
+    return getSHA(rand)
 
 
 def getSHA(data):
-	return hashlib.sha256(data).hexdigest()
+    return hashlib.sha256(data).hexdigest()
+
 
 def getBlogIPNS(id):
-	blog = Blog.get(id)
-	return blog.ipns
+    blog = Blog.get(id)
+    return blog.ipns
 
 
 def blogExists(name):
-	blog = Blog.query.filter_by(name=name).first()
-	return True if blog else False
+    blog = Blog.query.filter_by(name=name).first()
+    return True if blog else False
 
 
 def authorExists(author):
-	blog = Blog.query.filter_by(author=author).first()
-	return True if blog else False
+    blog = Blog.query.filter_by(author=author).first()
+    return True if blog else False
 
 
 def keyExists(blog_key):
-	blog_key = getSHA(blog_key.encode('utf-8'))
-	blog = Blog.query.filter_by(key=blog_key).first()
-	return True if blog else False
+    blog_key = getSHA(blog_key.encode('utf-8'))
+    blog = Blog.query.filter_by(key=blog_key).first()
+    return True if blog else False
 
 
 def isBlog(name, hash):
-	blog = Blog.query.filter_by(ipns=hash).first()
-	return True if blog else False
+    blog = Blog.query.filter_by(ipns=hash).first()
+    return True if blog else False
 
 
 def validateSubmission(blog_name, author_name):
-	if blogExists(blog_name):
-		return 'blog_exists'	
-	if authorExists(author_name):
-		return 'author_exists'
-	return False
+    if blogExists(blog_name):
+        return 'blog_exists'
+    if authorExists(author_name):
+        return 'author_exists'
+    return False
