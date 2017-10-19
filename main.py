e221a85afd1f45982d216349ac5754c96385ba26
PolBaladas <polbaladasluna@gmail.com>
2017-10-19 17:20:23 +0200

    Modified and prepared brb page

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import os
 app = Flask(__name__)
 app.config.from_pyfile('utils/config.py')
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-from utils import models, db_handler
-db.create_all()
+#db = SQLAlchemy(app)
+#migrate = Migrate(app, db)
+#from utils import models, db_handler
+#db.create_all()
 
 
 @app.route('/')
@@ -37,70 +37,70 @@ def donate():
     return render_template('donate.html')
 
 
-@app.route('/submission/')
-def submission():
-    return render_template('submission.html')
+# @app.route('/submission/')
+# def submission():
+#     return render_template('submission.html')
 
 
-@app.route('/<author>/')
-def blog(author):
-    blog = db_handler.getBlogByAuthor(author)
-    print(blog)
-    if blog:
-        return redirect(values.gateway + blog.hash + '/')
-    else:
-        return render_template('error.html', error='Bad Name')
+# @app.route('/<author>/')
+# def blog(author):
+#     blog = db_handler.getBlogByAuthor(author)
+#     print(blog)
+#     if blog:
+#         return redirect(values.gateway + blog.hash + '/')
+#     else:
+#         return render_template('error.html', error='Bad Name')
 
 
-@app.route('/newpost/', methods=['POST'])
-def newpost():
-    form = request.form
-    text = form['text']
-    title = form['title']
-    key = form['key']
-    blog = db_handler.getBlogByKey(key)
-    if blog:
-        post_hash = db_handler.newPost(title, text, blog.id)
-        return render_template(
-            'submission.html',
-            post_hash=post_hash,
-            blog_hash=blog.hash,
-            blog_name=blog.name,
-            author=blog.author
-        )
-    else:
-        return render_template('error.html', error='Sorry! Bad key.')
+# @app.route('/newpost/', methods=['POST'])
+# def newpost():
+#     form = request.form
+#     text = form['text']
+#     title = form['title']
+#     key = form['key']
+#     blog = db_handler.getBlogByKey(key)
+#     if blog:
+#         post_hash = db_handler.newPost(title, text, blog.id)
+#         return render_template(
+#             'submission.html',
+#             post_hash=post_hash,
+#             blog_hash=blog.hash,
+#             blog_name=blog.name,
+#             author=blog.author
+#         )
+#     else:
+#         return render_template('error.html', error='Sorry! Bad key.')
 
 
-@app.route('/newblog/', methods=['GET', 'POST'])
-def newblog():
-    if request.method == 'POST':
-        form = request.form
-        blog_name = form['blog_name']
-        author = form['author']
+# @app.route('/newblog/', methods=['GET', 'POST'])
+# def newblog():
+#     if request.method == 'POST':
+#         form = request.form
+#         blog_name = form['blog_name']
+#         author = form['author']
 
-        error = db_handler.validateSubmission(blog_name, author)
-        if error:
-            return render_template('error.html', error=error)
+#         error = db_handler.validateSubmission(blog_name, author)
+#         if error:
+#             return render_template('error.html', error=error)
 
-        key, hash = db_handler.newBlog(author, blog_name)
-        author = author.replace(' ','-')
-        return render_template('new_blog.html', key=key, adress=hash, author=author)
+#         key, hash = db_handler.newBlog(author, blog_name)
+#         author = author.replace(' ','-')
+#         return render_template('new_blog.html', key=key, adress=hash, author=author)
 
-    else:
-        return render_template('new_blog.html')
-
-
-@app.route('/post/<post_hash>/')
-def post(post_hash):
-    return redirect('https://ipfs.io/ipfs/' + post_hash + '/', code=302)
+#     else:
+#         return render_template('new_blog.html')
 
 
-@app.route('/checkBlogName/<blog_name>/')
-def checkBlogName(blog_name):
-    return str(db_handler.blogExists(blog_name))
+# @app.route('/post/<post_hash>/')
+# def post(post_hash):
+#     return redirect('https://ipfs.io/ipfs/' + post_hash + '/', code=302)
 
 
-@app.route('/checkAuthor/<author>/')
-def checkAuthor(author):
-    return str(db_handler.authorExists(author))
+# @app.route('/checkBlogName/<blog_name>/')
+# def checkBlogName(blog_name):
+#     return str(db_handler.blogExists(blog_name))
+
+
+# @app.route('/checkAuthor/<author>/')
+# def checkAuthor(author):
+#     return str(db_handler.authorExists(author))
